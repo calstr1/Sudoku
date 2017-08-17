4215c8802b81fb57b3df074ad32d6b181828ca0d
Callum Hussey <callumhussey1995@gmail.com>
2017-08-18 11:44:38 +1200

    Set up Game to allow user to choose a box for a value

--- Sudoku/Game.cs
+++ Sudoku/Game.cs
@@ -8,10 +8,34 @@ namespace Sudoku
 {
     class Game
     {
-        public static Board unsolved = new Board();
-        public static void Main(String[] args)
+        public static Board Unsolved = new Board();
+        public static void Play()
         {
+            while (Unsolved.board != Logic.Solved.board)
+            {
+                Unsolved.PrintBoard();
+                int index = GetIndex();
+            }
+        }
 
+        public static int GetIndex()//converts a set of coordinates to a board index
+        {
+            int[] coords = new int[2];
+            Console.WriteLine("Which column (1-9)?");
+            coords[0] = GetCoords()-1;
+            Console.WriteLine("Which row (1-9)?");
+            coords[1] = GetCoords()-1;
+            return coords[1]*9 + coords[0];
+        }
+
+        public static int GetCoords()//runs until a col or rown number is given in the correct format
+        {
+            string input = Console.ReadLine();
+            while (!new List<string>(new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9" }).Contains(input)){
+                Console.WriteLine("Please enter a number between 1 and 9: ");
+                input = Console.ReadLine();
+            }
+            return Convert.ToInt32(input);
         }
     }
 }
