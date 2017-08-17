fbd13659d513670de86a06221f61f950a089baf8
Callum Hussey <callumhussey1995@gmail.com>
2017-08-17 23:03:46 +1200

    Refactor for Game and Logic switch

--- Sudoku/Board.cs
+++ Sudoku/Board.cs
@@ -14,19 +14,19 @@ namespace Sudoku
         public static int[,] columns = new int[9, 9];
         public static int[,] squares = new int[9, 9];
         public static int[] board = new int[81];
-        
+
         public static void Fill(int[] input)//fills out the board
         {
             board = input;
             int num1 = input[0];
-            if (num1 == 0) Game.zeroes.Add(0);
+            if (num1 == 0) Logic.zeroes.Add(0);
             columns[0, 0] = input[0];
             rows[0, 0] = input[0];
             squares[0, 0] = input[0];
             for (int i = 1; i < 81; i++)
             {
                 int num = input[i];
-                if (num == 0) Game.zeroes.Add(i);
+                if (num == 0) Logic.zeroes.Add(i);
                 int row = i / 9;
                 int col = i % 9;
                 int squ = (3 * (row / 3)) + (col / 3);
