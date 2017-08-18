acb8b5d90aa3fd7e1ecb993ef809b346d3b86504
Callum Hussey <callumhussey1995@gmail.com>
2017-08-18 12:05:27 +1200

    update debug files

--- Sudoku/Game.cs
+++ Sudoku/Game.cs
@@ -15,6 +15,8 @@ namespace Sudoku
             {
                 Unsolved.PrintBoard();
                 int index = GetIndex();
+                Console.WriteLine("Enter a value for this box (1-9): ");
+                int value = GetOneNine();//gets a value for the box
             }
         }
 
@@ -22,13 +24,13 @@ namespace Sudoku
         {
             int[] coords = new int[2];
             Console.WriteLine("Which column (1-9)?");
-            coords[0] = GetCoords()-1;
+            coords[0] = GetOneNine()-1;
             Console.WriteLine("Which row (1-9)?");
-            coords[1] = GetCoords()-1;
+            coords[1] = GetOneNine()-1;
             return coords[1]*9 + coords[0];
         }
 
-        public static int GetCoords()//runs until a col or rown number is given in the correct format
+        public static int GetOneNine()//runs until a number between 1 and 9 is given in the correct format
         {
             string input = Console.ReadLine();
             while (!new List<string>(new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9" }).Contains(input)){
